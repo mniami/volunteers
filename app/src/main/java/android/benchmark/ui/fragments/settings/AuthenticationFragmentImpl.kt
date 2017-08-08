19a3bfd3f323790c3d7ca7cee9440a63674fd977
mniami <d.szczepek@gmail.com>
2017-08-08 17:00:56 +0200

    #4 changed facebook api keys

--- app/src/main/java/android/benchmark/ui/fragments/settings/AuthenticationFragmentImpl.kt
+++ app/src/main/java/android/benchmark/ui/fragments/settings/AuthenticationFragmentImpl.kt
@@ -4,6 +4,7 @@ import android.benchmark.R
 import android.benchmark.helpers.Services
 import android.benchmark.ui.fragments.base.BaseFragment
 import android.benchmark.ui.fragments.base.FragmentConfiguration
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import com.facebook.CallbackManager
@@ -11,10 +12,12 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import kotlinx.android.synthetic.main.authentication_fragment.*
 
 class AuthenticationFragmentImpl : BaseFragment<AuthenticationPresenter>(), AuthenticationFragment {
-
+    val TAG = "AuthenticationFragment"
     init {
         configuration = FragmentConfiguration.withLayout(R.layout.authentication_fragment)
                 .title(R.string.authentication)
@@ -33,11 +36,14 @@ class AuthenticationFragmentImpl : BaseFragment<AuthenticationPresenter>(), Auth
         facebookLoginButton?.setReadPermissions("email", "public_profile")
         facebookLoginButton?.registerCallback(mCallbackManager, object : FacebookCallback<LoginResult> {
             override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "Facebook login successful")
                 presenter?.onAuthenticationSuccess(loginResult)
             }
 
             override fun onCancel() {}
-            override fun onError(var1: FacebookException) {}
+            override fun onError(var1: FacebookException) {
+                Log.d(TAG, "Facebook login error, ${var1.message}")
+            }
         })
     }
 }
